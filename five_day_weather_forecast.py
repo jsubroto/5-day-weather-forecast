3896bc458e6a7622a48a9d11d46c4b4b963434e3
Jaimes Subroto <jaimessubroto@gmail.com>
2024-09-21 17:06:26 -0700

    Replaced single quotes to double quotes for strings

--- five_day_weather_forecast.py
+++ five_day_weather_forecast.py
@@ -1,12 +1,12 @@
 import requests
 import calendar
 
-api_key = '<your_api_key>'
-api_call = 'https://api.openweathermap.org/data/2.5/forecast?appid=' + api_key
+api_key = "<your_api_key>"
+api_call = "https://api.openweathermap.org/data/2.5/forecast?appid=" + api_key
 
 running = True
 
-print('Welcome to Jaimes Subroto\'s 5 day weather forecast application using OpenWeatherMap\'s API!')
+print("Welcome to Jaimes Subroto's 5 day weather forecast application using OpenWeatherMap's API!")
 
 # Program loop
 while running:
@@ -16,51 +16,52 @@ while running:
 
         # Input validation
         try:
-            print('\nThis application supports search by city(0) or search by zip code(1).')
-            search = int(input('Please input 0 or 1: '))
+            print(
+                "\nThis application supports search by city(0) or search by zip code(1).")
+            search = int(input("Please input 0 or 1: "))
         except ValueError:
             print("Sorry, I didn't understand that.")
         else:
 
             # Passed the validation test
             if search == 0:
-                city = input('Please input the city name: ')
-                if city.lower() == 'sf':
-                    city = 'San Francisco, US'
+                city = input("Please input the city name: ")
+                if city.lower() == "sf":
+                    city = "San Francisco, US"
 
                 # Appends the city to the api call
-                api_call += '&q=' + city
+                api_call += "&q=" + city
                 break
-                
+
             elif search == 1:
-                zip_code = input('Please input the zip code: ')
-                
+                zip_code = input("Please input the zip code: ")
+
                 # Appends the zip code to the api call
-                api_call += '&zip=' + zip_code
+                api_call += "&zip=" + zip_code
                 break
-                
+
             else:
                 # Prints the invalid number (not 0 or 1)
-                print('{} is not a valid option.'.format(search))
+                print("{} is not a valid option.".format(search))
 
     # Stores the Json response
     json_data = requests.get(api_call).json()
 
     location_data = {
-        'city': json_data['city']['name'],
-        'country': json_data['city']['country']
+        "city": json_data["city"]["name"],
+        "country": json_data["city"]["country"]
     }
 
-    print('\n{city}, {country}'.format(**location_data))
+    print("\n{city}, {country}".format(**location_data))
 
     # The current date we are iterating through
     current_date = ''
 
     # Iterates through the array of dictionaries named list in json_data
-    for item in json_data['list']:
+    for item in json_data["list"]:
 
         # Time of the weather data received, partitioned into 3 hour blocks
-        time = item['dt_txt']
+        time = item["dt_txt"]
 
         # Split the time into date and hour [2018-04-15 06:00:00]
         next_date, hour = time.split(' ')
@@ -70,8 +71,8 @@ while running:
             current_date = next_date
             year, month, day = current_date.split('-')
             date = {'y': year, 'm': month, 'd': day}
-            print('\n{m}/{d}/{y}'.format(**date))
-        
+            print("\n{m}/{d}/{y}".format(**date))
+
         # Grabs the first 2 integers from our HH:MM:SS string to get the hours
         hour = int(hour[:2])
 
@@ -79,40 +80,41 @@ while running:
         if hour < 12:
             if hour == 0:
                 hour = 12
-            meridiem = 'AM'
+            meridiem = "AM"
         else:
             if hour > 12:
                 hour -= 12
-            meridiem = 'PM'
+            meridiem = "PM"
 
         # Prints the hours [HH:MM AM/PM]
-        print('\n%i:00 %s' % (hour, meridiem))
+        print("\n%i:00 %s" % (hour, meridiem))
 
         # Temperature is measured in Kelvin
-        temperature = item['main']['temp']
+        temperature = item["main"]["temp"]
 
         # Weather condition
-        description = item['weather'][0]['description'],
+        description = item["weather"][0]["description"],
 
         # Prints the description as well as the temperature in Celcius and Farenheit
-        print('Weather condition: %s' % description)
-        print('Celcius: {:.2f}'.format(temperature - 273.15))
-        print('Farenheit: %.2f' % (temperature * 9/5 - 459.67))
+        print("Weather condition: %s" % description)
+        print("Celcius: {:.2f}".format(temperature - 273.15))
+        print("Farenheit: %.2f" % (temperature * 9/5 - 459.67))
 
     # Prints a calendar of the current month
     calendar = calendar.month(int(year), int(month))
-    print('\n'+ calendar)
+    print('\n' + calendar)
 
     # Asks the user if he/she wants to exit
     while True:
-        running = input('Anything else we can help you with? ')
-        if running.lower() == 'yes' or running.lower() == 'y':
-            print('Great!')
+        running = input("Anything else we can help you with? ")
+        if running.lower() == "yes" or running.lower() == 'y':
+            print("Great!")
             break
-        elif running.lower() == 'no' or running.lower() == 'n' or running == 'exit':
-            print('Thank you for using Jaimes Subroto\'s 5 day weather forecast application.')
-            print('Have a great day!')
+        elif running.lower() == "no" or running.lower() == 'n' or running == "exit":
+            print(
+                "Thank you for using Jaimes Subroto's 5 day weather forecast application.")
+            print("Have a great day!")
             running = False
             break
         else:
-            print('Sorry, I didn\'t get that.')
+            print("Sorry, I didn't get that.")
